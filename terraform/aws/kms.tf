6ad7111b5f70af374283d6af75028577493627c4
nbarhamwiz <nick.barham@wiz.io>
2024-09-06 13:27:43 +0100

    Update kms.tf

--- terraform/aws/kms.tf
+++ terraform/aws/kms.tf
@@ -6,7 +6,7 @@ resource "aws_kms_key" "logs_key" {
   tags = {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/kms.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_at = "2020-06-16 14:46:21"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
